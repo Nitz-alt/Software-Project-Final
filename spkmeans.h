a1b630e05688b9cd4f52346e6fb03594e8faa8fe
Nitz-alt <nitzanyizhar@gmail.com>
2022-07-20 15:38:13 +0300

    Implemeted wam connection from python. Commit before trying to fix Linkage problem

--- spkmeans.h
+++ spkmeans.h
@@ -1,8 +1,12 @@
 #ifndef SPKMEANS_H_
 #define SPKMEANS_H_
-double **kmeans(double ** , double ** , int const , int const , int const , double const , int const );
-double **wam(double **, size_t , size_t);
-double ** ddg(double **, size_t , size_t);
-double **lnorm(double ** , size_t , size_t);
-double ** jacobi(double **, size_t);
+double **kmeans(double ** centeroids, double ** vectors, int const length, int const numberOfVectors, int const K, double const EPSILON, int const MAX_ITER);
+double **wam(double **matrix, size_t numberOfVectors, size_t length);
+double ** ddg(double **matrix, size_t numberOfVectors, size_t length);
+double **lnorm(double ** vectors, size_t numberOfVectors, size_t length);
+double ** jacobi(double **vectors, size_t dim);
+double **createBlockMatrix(size_t size, size_t numberOfRows, size_t numberOfColumns);
+void freeBlock(double **ptr);
+void errorMsg(int code);
+void printMatrix(double **array, size_t numberOfRows, size_t numberOfColumns);
 #endif
